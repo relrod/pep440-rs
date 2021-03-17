04b6bf0f8994fe86ff0b870587e72ec290cd8e26
relrod <rick@elrod.me>
2021-03-17 04:06:32 +0000

    Deploying to gh-pages from @ relrod/pep440-rs@c86b9c26473330ae41ffcbaa23614c2b9cde1dde 🚀

--- implementors/core/cmp/trait.PartialOrd.js
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pep440"] = [{"text":"impl PartialOrd&lt;Version&gt; for Version","synthetic":false,"types":[]}];
+implementors["pep440"] = [{"text":"impl PartialOrd&lt;Version&gt; for Version","synthetic":false,"types":[]},{"text":"impl PartialOrd&lt;LocalVersion&gt; for LocalVersion","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
